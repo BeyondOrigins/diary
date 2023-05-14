845afbe299e6451b88364a74a6b4a092226a4315
BeyondOrigins <dima.a.ivlev@gmail.com>
2023-05-14 12:08:28 +0300

    edited .gitignore

---
+++ config.py
@@ -0,0 +1,12 @@
+DEFAULT_AVATAR_PATH = "/static/styles/img/default_avatar.png"
+WEEKDAYS = {
+    0 : "Понедельник",
+    1 : "Вторник",
+    2 : "Среда",
+    3 : "Четверг",
+    4 : "Пятница",
+    5 : "Суббота",
+    6 : "Воскресенье",
+}
+
+APP_PASSWORD = "qtlbrizlzynyxgll"
